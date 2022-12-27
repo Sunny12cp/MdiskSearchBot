2f224b046642df6ee6647db80204ac9f6076df27
Sunny12cp <114519415+Sunny12cp@users.noreply.github.com>
2022-12-27 17:59:30 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo...."
-git clone https://github.com/RoyalKrrishna/MdiskWalaBot.git /MdiskWalaBot
+git clone https://github.com/Sunny12cp/MdiskWalaBot.git /MdiskWalaBot
 cd /MdiskWalaBot
 pip3 install -r requirements.txt
 echo "Starting Bot...."
